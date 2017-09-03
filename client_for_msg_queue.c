f63855c577c93f4d9ca677103de86f4092083248
Anmol1696 <anmol1696@gmail.com>
2017-09-03 14:52:36 +0530

    Client side code on backend for receiving uuid from msg queue

---
+++ client_for_msg_queue.c
@@ -0,0 +1,45 @@
+#include "client_for_msg_queue.h"
+
+int init_sockfd(){ 
+    int sockfd = 0;
+    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){ 
+        printf("\nERROR: Could not create socket\n");
+        return -1; 
+    }   
+        
+    struct sockaddr_in serv_addr;
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+    serv_addr.sin_addr.s_addr = inet_addr(SERVER_IP_ADDR);
+
+    if(connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){ 
+        printf("\nERROR : Connect Failed\n\n");
+        return -1; 
+    }   
+
+    return sockfd;
+}
+
+char* get_uuid_str_from_msg_queue(int sockfd){
+    char frontend_or_msgqueue = 'm';
+    write(sockfd, &frontend_or_msgqueue, sizeof(char));
+    
+    char* uuid_str = malloc(UUID_SIZE_FOR_STR);
+    int br = read(sockfd, uuid_str, UUID_SIZE_FOR_STR);
+
+    if(br < UUID_SIZE_FOR_STR){
+        printf("Incorrect Read");
+    }
+
+    printf("UUID read as -> %s\n", uuid_str);
+
+    close(sockfd);
+
+    return uuid_str;
+}
+
+int main(int argc, char** argv){
+    int sockfd = init_sockfd();
+    char* uuid_str = get_uuid_str_from_msg_queue(sockfd);
+    //if(*uuid_str == EMPTY_STR){printf("Message Queue Empty\n");}
+}
---
+++ client_for_msg_queue.h
@@ -0,0 +1,19 @@
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <uuid/uuid.h>
+#include <netdb.h>
+
+#define SERVER_IP_ADDR "127.0.0.1"
+#define PORT 5000
+#define UUID_SIZE_FOR_STR 32
+#define EMPTY_STR "00000000000000000000000000000000"
+
+int init_sockfd();
+char* get_uuid_str_from_msg_queue(int connfd);
